2cbc3eb083afc33ac124ccc9032c5e44fada6831
SamKlep <shamdemann23@gmail.com>
2021-01-20 17:59:04 -0600

    Carousel styled & Media Queries.

--- src/components/home/Services.js
+++ src/components/home/Services.js
@@ -25,12 +25,6 @@ const Services = () => {
                   src='/img/house2.jpg'
                   alt='First slide'
                 />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
-                  </p>
-                </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
                 <img
@@ -38,13 +32,6 @@ const Services = () => {
                   src='/img/demo15.jpg'
                   alt='Third slide'
                 />
-
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-                </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
                 <img
@@ -52,14 +39,6 @@ const Services = () => {
                   src='/img/dirt4.jpg'
                   alt='Third slide'
                 />
-
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>
-                    Praesent commodo cursus magna, vel scelerisque nisl
-                    consectetur.
-                  </p>
-                </Carousel.Caption>
               </Carousel.Item>
             </Carousel>
           </Col>
